5cc6df99c920a71c45e651e589aced9a9aa08027
Daddy de Lucía <fco@Daddys-MacBook-Pro.local>
2023-03-26 00:50:56 -0300

    CountingBits

---
+++ workspaces/fcopardo/app/src/main/kotlin/fcopardo/array/CountingBits.kt
@@ -0,0 +1,31 @@
+package app.src.main.kotlin.fcopardo.array
+
+class CountingBits {
+    companion object{
+        fun countBits(n: Int): IntArray {
+            var c = 0
+            var myArray = IntArray(n+1)
+            while(c<=n){
+                myArray[c] = onesInBinary(c)
+                c++
+            }
+            return myArray
+        }
+
+        fun onesInBinary(num : Int) : Int {
+            var rest = num
+            var total = ""
+            var digit = 0
+            var accumulated = 0
+            while(rest > 0){
+                digit = (rest % 2)
+                if(digit == 1){
+                    accumulated++
+                }
+                total += digit.toString()
+                rest /= 2
+            }
+            return accumulated
+        }
+    }
+}
