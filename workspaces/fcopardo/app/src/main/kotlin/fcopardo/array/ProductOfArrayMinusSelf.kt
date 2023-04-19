8cfb73cc134247d671c1020d7c75b33eab7f4b01
Daddy de Lucía <fco@Daddys-MacBook-Pro.local>
2023-04-18 21:27:57 -0400

    improvement for runtime

--- workspaces/fcopardo/app/src/main/kotlin/fcopardo/array/ProductOfArrayMinusSelf.kt
+++ workspaces/fcopardo/app/src/main/kotlin/fcopardo/array/ProductOfArrayMinusSelf.kt
@@ -2,59 +2,74 @@ package app.src.main.kotlin.fcopardo.array
 
 class ProductOfArrayMinusSelf {
     companion object{
-        class Solution {
+        var calculated = mutableMapOf<Int, Int>()
+        var calculatedTop = mutableMapOf<Int, Int>()
 
-            var calculated = mutableMapOf<Int, Int>()
+        fun productExceptSelf(nums: IntArray): IntArray {
+            var c = 0
+            var copy = IntArray(nums.size)
+            create(nums)
+            while(c<=nums.lastIndex){
+                copy[c] = multiply(nums, c)
+                c++
+            }
+            return copy
 
-            fun productExceptSelf(nums: IntArray): IntArray {
-                var c = 0
-                var copy = IntArray(nums.size)
-                create(nums)
-                while(c<=nums.lastIndex){
-                    copy[c] = multiply(nums, c)
-                    c++
+        }
+
+        fun create(nums:IntArray){
+            var c = 0
+            var k = nums.lastIndex
+
+            var result = 1
+            var aux = 1
+
+            var result2 = 1
+
+            while(c<=nums.lastIndex){
+                result = result*nums[c]
+                if(c!=0){
+                    aux = aux*nums[c]
                 }
-                return copy
-
+                if(!calculated.containsKey(c)){
+                    calculated[c] = result
+                }
+                c++
             }
 
-            fun create(nums:IntArray){
-                var c = 0
-                var result = 1
-                var aux = 1
-                while(c<=nums.lastIndex){
-                    result = result*nums[c]
-                    if(c!=0){
-                        aux = aux*nums[c]
-                    }
-                    if(!calculated.containsKey(c)){
-                        calculated[c] = result
-                    }
-                    c++
+            while(k>=0){
+                result2 = result2*nums[k]
+                if(!calculatedTop.containsKey(k)){
+                    calculatedTop[k] = result2
                 }
-                calculated[nums.lastIndex+1] = aux
+                k--
             }
 
-            fun multiply(nums:IntArray, excluded: Int) : Int {
-                var c = 0
-                var result = 1
+            calculated[nums.lastIndex+1] = aux
+        }
 
-                if(excluded == 0) {
-                    return calculated[nums.lastIndex+1]!!
-                }
+        fun multiply(nums:IntArray, excluded: Int) : Int {
+            var c = 0
+            var result = 1
 
-                if( (calculated.containsKey(excluded-1)) ){
-                    result = calculated[excluded-1]!!
-                    c = excluded+1
-                }
-
-                while(c<=nums.lastIndex){
-                    result = result*nums[c]
-                    c++
-                }
-
-                return result
+            if(excluded == 0) {
+                return calculated[nums.lastIndex+1]!!
             }
+
+            if( (calculated.containsKey(excluded-1)) ){
+                result = calculated[excluded-1]!!
+                c = excluded+1
+                if(calculatedTop.containsKey(c)){
+                    return result*calculatedTop[c]!!
+                }
+            }
+
+            while(c<=nums.lastIndex){
+                result = result*nums[c]
+                c++
+            }
+
+            return result
         }
     }
 }
